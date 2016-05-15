cb7cb30553876df7332359478f9c64902c9e8d3d
yws528 <yws528@foxmail.com>
2016-05-15 23:25:29 +0800

    CriminalIntent
    The 1st lesson to learn Fragment.

---
+++ CriminalIntent/src/com/example/criminalintent/CrimeFragment.java
@@ -0,0 +1,124 @@
+package com.example.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+	public static final String EXTRA_CRIME_ID = "com.example.criminalintent.crime_id";
+
+	private static final String DIALOG_DATE = "date";
+	private static final int REQUEST_DATE = 0;
+
+	private Crime mCrime;
+	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
+
+	public static CrimeFragment newInstance(UUID crimeId) {
+		Bundle args = new Bundle();
+		args.putSerializable(EXTRA_CRIME_ID, crimeId);
+
+		CrimeFragment fragment = new CrimeFragment();
+		fragment.setArguments(args);
+
+		return fragment;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        // UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        UUID crimeId = (UUID) getArguments().getSerializable(EXTRA_CRIME_ID);
+		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, container, false);
+		
+		mTitleField = (EditText) v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				mCrime.setTitle(s.toString());
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		mDateButton = (Button) v.findViewById(R.id.crime_date);
+		updateDate();
+		mDateButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FragmentManager fm = getActivity().getSupportFragmentManager();
+				DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+				dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+				dialog.show(fm, DIALOG_DATE);
+				
+			}
+		});
+		
+		mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setChecked(mCrime.isSolved());
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// Set the crime's solved property
+				mCrime.setSolved(isChecked);
+			}
+		});
+		return v;
+	}
+
+	public void returnResult() {
+		getActivity().setResult(Activity.RESULT_OK, null);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (Activity.RESULT_OK != resultCode) return;
+
+		if (REQUEST_DATE == requestCode) {
+			Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+			mCrime.setDate(date);
+			updateDate();
+		}
+	}
+
+	private void updateDate() {
+		mDateButton.setText(mCrime.getDate().toString());
+	}
+}
---
+++ CriminalIntent/src/com/example/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,75 @@
+package com.example.criminalintent;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+public class CrimePagerActivity extends FragmentActivity {
+	private ViewPager mViewPager;
+	private ArrayList<Crime> mCrimes;
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle saveInstanceState) {
+		super.onCreate(saveInstanceState);
+		mViewPager = new ViewPager(this);
+		mViewPager.setId(R.id.viewPager);
+		// mViewPager.setOffscreenPageLimit(limit);
+		setContentView(mViewPager);
+
+		mCrimes = CrimeLab.get(this).getCrimes();
+
+		FragmentManager fm = getSupportFragmentManager();
+		mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+
+			@Override
+			public int getCount() {
+				return mCrimes.size();
+			}
+
+			@Override
+			public Fragment getItem(int pos) {
+				Crime crime = mCrimes.get(pos);
+				return CrimeFragment.newInstance(crime.getId());
+			}
+		});
+
+		mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int pos) {
+				Crime crime = mCrimes.get(pos);
+				if (null != crime.getTitle()) {
+					setTitle(crime.getTitle());
+				}
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		UUID crimeId = (UUID) getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        for (int i = 0; i < mCrimes.size(); i++) {
+        	if (mCrimes.get(i).getId().equals(crimeId)) {
+        		mViewPager.setCurrentItem(i);
+        		break;
+        	}
+        }
+	}
+
+}
